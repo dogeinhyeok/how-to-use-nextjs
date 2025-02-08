4e9ecd001fa2074267d114b4400d2173cdb2cb50
devinhyeok <devinhyeok@gmail.com>
2025-02-08 20:05:41 +0900

    feat/streaming-and-error

--- app-router/src/app/(with-searchbar)/page.tsx
+++ app-router/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,12 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -22,13 +26,12 @@ async function AllBooks() {
 }
 
 async function RecommendBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
   );
-  if (!response.ok) {
-    return <div>오류가 발생했습니다...</div>;
-  }
+
   const recoBooks: BookData[] = await response.json();
 
   return (
@@ -40,16 +43,22 @@ async function RecommendBooks() {
   );
 }
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecommendBooks />
+        <Suspense fallback={<BookListSkeleton count={3} />}>
+          <RecommendBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<BookListSkeleton count={10} />}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- app-router/src/app/(with-searchbar)/search/page.tsx
+++ app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,14 +1,27 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 
-// export const dynamic = "force-dynamic";
-/**
- * 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
- * 1. auto: 기본값
- * 2. force-static: 모든 페이지를 Static 페이지로 설정
- * 3. force-dynamic: 모든 페이지를 Dynamic 페이지로 설정
- * 4. error: 유효하지 않은 설정
- */
+async function SearchResult({ q }: { q: string }) {
+  await delay(1500);
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: "force-cache" }
+  );
+
+  const books: BookData[] = await response.json();
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default async function Page({
   searchParams,
@@ -18,21 +31,9 @@ export default async function Page({
   }>;
 }) {
   const { q } = await searchParams;
-
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
-    { cache: "force-cache" }
-  );
-  if (!response.ok) {
-    return <div>오류가 발생했습니다...</div>;
-  }
-  const books: BookData[] = await response.json();
-
   return (
-    <div>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
-    </div>
+    <Suspense key={q} fallback={<BookListSkeleton count={10} />}>
+      <SearchResult q={q || ""} />
+    </Suspense>
   );
 }
